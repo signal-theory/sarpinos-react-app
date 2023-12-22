cf1d5d21446a3eb800db25a56570e01627e2561e
Beth Wickerson <beth.wickerson@signaltheory.com>
2023-12-22 00:03:47 -0600

    company info

--- src/pages/about/company/Company.jsx
+++ src/pages/about/company/Company.jsx
@@ -2,15 +2,24 @@ import React, { useEffect, useState } from 'react'
 import SEOHelmet from '../../../components/SEOHelmet'
 import './Company.css'
 import axios from 'axios'
+import { Link } from 'react-router-dom';
 import Loading from '../../../components/Loading'
+import basilImg_1 from '../../../assets/basil-leaf-1.png'
+import basilImg_2 from '../../../assets/basil-leaf-2.png'
+import basilImg_3 from '../../../assets/basil-leaf-3.png'
+import basilImg_4 from '../../../assets/basil-leaf-4.png'
 
 const Company = () => {
 
   const [featuredImage, setFeaturedImage] = useState(null);
   const [featuredImageAlt, setFeaturedImageAlt] = useState(null);
   const [pageSlug, setPageSlug] = useState(null);
-  const [pageTitle, setPageTitle] = useState(null);
   const [pageContent, setPageContent] = useState(null);
+  const [gourmetHeadline, setGourmetHeadline] = useState(null);
+  const [gourmetParagraph, setGourmetParagraph] = useState(null);
+  const [gourmetIngredients1, setGourmetIngredients1] = useState(null);
+  const [gourmetIngredients2, setGourmetIngredients2] = useState(null);
+  const [gourmetIngredients3, setGourmetIngredients3] = useState(null);
   const [seoData, setSeoData] = useState({});
 
   const [isLoading, setIsLoading] = useState(true);
@@ -23,8 +32,12 @@ const Company = () => {
         const post = response.data; // Get the post data
 
         setPageSlug(post.slug);
-        setPageTitle(post.title.rendered); // Set the page title
         setPageContent(post.content.rendered); // Set the page content
+        setGourmetHeadline(post.acf.gourmet_headline); // Set the gourmet headline
+        setGourmetParagraph(post.acf.gourmet_paragraph); // Set the gourmet paragraph
+        setGourmetIngredients1(post.acf.gourmet_ingredients_1); // Set the gourmet ingredients 1
+        setGourmetIngredients2(post.acf.gourmet_ingredients_2); // Set the gourmet ingredients 2
+        setGourmetIngredients3(post.acf.gourmet_ingredients_3); // Set the gourmet ingredients 3
 
         // Set the SEO data
         if (post.yoast_head_json) {
@@ -62,10 +75,51 @@ const Company = () => {
         <div className="full-page-container company-page inner-hero">
           <div className="responsive-column-container">
             <div className="featured-image">
-              {featuredImage && <img src={featuredImage} alt={featuredImageAlt} />}
+              <div className="basil-animation">
+                {featuredImage && <img src={featuredImage} alt={featuredImageAlt} />}
+                <img src={basilImg_1} className="parallax basil-leaf-1" alt="basil leaf" />
+                <img src={basilImg_2} className="parallax basil-leaf-2" alt="basil leaf" />
+                <img src={basilImg_3} className="parallax basil-leaf-3" alt="basil leaf" />
+                <img src={basilImg_4} className="parallax basil-leaf-4" alt="basil leaf" />
+                <img src={basilImg_1} className="parallax basil-leaf-5" alt="basil leaf" />
+                <img src={basilImg_2} className="parallax basil-leaf-6" alt="basil leaf" />
+              </div>
             </div>
             <div className="content">
               {pageContent && <div dangerouslySetInnerHTML={{ __html: pageContent }} />}
+              <Link to="" className="btn primary-btn"><span>Order Online</span></Link>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="viewport cream-color">
+        <div className="page-container gourmet-specials red-color inner-hero text-align-center">
+          <div className="specials-container" style={{ padding: '5rem 2rem' }}>
+            {gourmetHeadline && <h2>{gourmetHeadline}</h2>}
+            {gourmetParagraph && <div dangerouslySetInnerHTML={{ __html: gourmetParagraph }} style={{ maxWidth: '500px', margin: '0 auto 2rem' }} />}
+            <hr />
+            <div className="responsive-three-column-container">
+              <div className="content">
+                {gourmetIngredients1 && <div dangerouslySetInnerHTML={{ __html: gourmetIngredients1 }} />}
+              </div>
+              <div className="content">
+                {gourmetIngredients2 && <div dangerouslySetInnerHTML={{ __html: gourmetIngredients2 }} />}
+              </div>
+              <div className="content">
+                {gourmetIngredients3 && <div dangerouslySetInnerHTML={{ __html: gourmetIngredients3 }} />}
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="viewport innerhero cream-color">
+        <div className="full-page-container company-page inner-hero">
+          <div className="responsive-column-container">
+            <div className="timeline">
+              // timeline
+            </div>
+            <div className="pizza">
+              //pizza
             </div>
           </div>
         </div>
