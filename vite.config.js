76a773fe524aeeda84d626cf234894083a3da944
Beth Wickerson <beth.wickerson@signaltheory.com>
2024-01-03 11:17:22 -0600

    revert to pre-ssr

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-const pkg = require('./package.json')
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: './src/entry-server.jsx' // Path to your server entry point
-    }
-  },
-  ssr: {
-    noExternal: Object.keys(pkg.dependencies || {})
-  }
 })
